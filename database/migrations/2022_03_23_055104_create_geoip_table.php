a579d23b96e9b9e21c8bcc873acbb8c38950ab9e
DadOwl <me@dadowl.dev>
2022-03-23 18:22:44 +0700

    add new migration

---
+++ database/migrations/2022_03_23_055104_create_geoip_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('geoip', function (Blueprint $table) {
+            $table->id();
+            $table->string("common_name");
+            $table->string("real_address");
+            $table->string("status_path");
+            $table->string("virtual_address");
+
+            $table->float("lat");
+            $table->float("long");
+
+            $table->boolean("is_deprecated")->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('geoip');
+    }
+};
