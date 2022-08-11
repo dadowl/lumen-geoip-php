1e44ab101a9a0b836b665d4b9d11f07509f75d2f
DadOwl <me@dadowl.dev>
2022-08-11 14:13:58 +0700

    fix some errors

--- app/Http/Controllers/GeoIpController.php
+++ app/Http/Controllers/GeoIpController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 //require_once 'vendor/autoload.php';
 use Exception;
 use GeoIp2\Database\Reader;
-use GeoIp2\Exception\AddressNotFoundException;
 use Illuminate\Http\Request;
-use MaxMind\Db\Reader\InvalidDatabaseException;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 class GeoIpController extends Controller
 {
+
+    public function __construct(Request $request)
+    {
+        $this->validate(
+            $request, [
+                'ip' => 'ip',
+                'locale' => 'min:2|max:3|in:de,en,es,fr,ja,ru',
+            ]
+        );
+    }
 
     /**
      * Provision a new web server.
@@ -20,26 +27,23 @@ class GeoIpController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $ip = app('request')->get("ip");
-        $envLocale = env('GEO_LOCALE', "en");
-        $locale = $envLocale;
+        $ip = app('request')->input("ip", null);
+        $locale = env('APP_LOCALE', "en");
 
-
-        if ($ip == null) {
+        if ($ip === null) {
             $ip = $request->server('REMOTE_ADDR');
         }
 
-        if (app('request')->get("locale") != null){
-            $locale = app('request')->get("locale");
+        if (app('request')->input("locale", null) !== null){
+            $locale = app('request')->input("locale", null);
         }
 
         try {
-            $reader = new Reader(storage_path('/app/GeoLite2-City.mmdb'));
+            $reader = new Reader(storage_path(env('GEO_DATABASE_PATH', "/app/GeoLite2-City.mmdb")));
         } catch (Exception $e) {
             return response()->json([
                 "status" => "error",
                 "message" => "Database not found.",
-                "data" => [],
                 "request"=>[
                     'ip' => $ip,
                     'locale' => $locale
@@ -53,7 +57,6 @@ class GeoIpController extends Controller
             return response()->json([
                 "status" => "error",
                 "message" => "IP not found.",
-                "data" => [],
                 "request"=>[
                     'ip' => $ip,
                     'locale' => $locale
@@ -62,7 +65,22 @@ class GeoIpController extends Controller
         }
 
         if (!array_key_exists($locale, $record->country->names)) {
-            $locale = $envLocale;
+            $locale = env('APP_LOCALE', "en");
+        }
+
+        if (!array_key_exists($locale, $record->country->names)) {
+            $locale = env('FALLBACK_LOCALE', "en");
+        }
+
+        if (!array_key_exists($locale, $record->country->names)) {
+            return response()->json([
+                "status" => "error",
+                "message" => "Locale not found.",
+                "request"=>[
+                    'ip' => $ip,
+                    'locale' => $locale
+                ]
+            ]);
         }
 
         return response()->json([
