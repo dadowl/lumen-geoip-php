f3ba69196f296951d24c9ef3ee2dae59c00d6ee7
DadOwl <me@dadowl.dev>
2022-08-11 13:05:25 +0700

    add request locale

--- app/Http/Controllers/GeoIpController.php
+++ app/Http/Controllers/GeoIpController.php
@@ -21,14 +21,16 @@ class GeoIpController extends Controller
     public function __invoke(Request $request)
     {
         $ip = app('request')->get("ip");
+        $envLocale = env('GEO_LOCALE', "en");
+        $locale = $envLocale;
+
 
         if ($ip == null) {
-            /*return response()->json([
-                "status" => "error",
-                "message" => "IP not transmitted.",
-                "data" => []
-            ]);*/
             $ip = $request->server('REMOTE_ADDR');
+        }
+
+        if (app('request')->get("locale") != null){
+            $locale = app('request')->get("locale");
         }
 
         try {
@@ -38,7 +40,10 @@ class GeoIpController extends Controller
                 "status" => "error",
                 "message" => "Database not found.",
                 "data" => [],
-                "request"=>[ 'ip' => $ip ]
+                "request"=>[
+                    'ip' => $ip,
+                    'locale' => $locale
+                ]
             ]);
         }
 
@@ -49,22 +54,32 @@ class GeoIpController extends Controller
                 "status" => "error",
                 "message" => "IP not found.",
                 "data" => [],
-                "request"=>[ 'ip' => $ip ]
+                "request"=>[
+                    'ip' => $ip,
+                    'locale' => $locale
+                ]
             ]);
+        }
+
+        if (!array_key_exists($locale, $record->country->names)) {
+            $locale = $envLocale;
         }
 
         return response()->json([
             "status" => "successful",
             "message" => "",
             "data" => [
-                "country" => $record->country->name,
-                "subdivision" => $record->mostSpecificSubdivision->name,
-                "city" => $record->city->name,
+                "country" => $record->country->names[$locale],
+                "subdivision" => $record->mostSpecificSubdivision->names[$locale],
+                "city" => $record->city->names[$locale],
                 "postal_code" => $record->postal->code,
                 "latitude" => $record->location->latitude,
                 "longitude" => $record->location->longitude
             ],
-            "request"=>[ 'ip' => $ip ]
+            "request"=>[
+                'ip' => $ip,
+                'locale' => $locale
+            ]
         ]);
     }
 
